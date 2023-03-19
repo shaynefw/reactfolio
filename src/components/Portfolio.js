48c4b99d9941457eb2ab0df9b13191516311e65d
swmac <shaynefw@gmail.com>
2023-03-19 17:49:25 -0400

    feat: added portfolio and project

---
+++ src/components/Portfolio.js
@@ -0,0 +1,63 @@
+import React from "react";
+import Project from "./partial-components/Project";
+import "../style/Portfolio.css";
+
+const projects = [
+  {
+    title: "GennyREADME",
+    image: "https://github.com/shaynefw/GennyREADME/raw/main/assets/images/gennySS.png",
+    appLink: "https://shaynefw.github.io/GennyREADME/",
+    repoLink: "https://github.com/shaynefw/GennyREADME",
+  },
+  {
+    title: "WikiGo",
+    image: "https://github.com/jjocelynn/WikiGo/raw/main/assets/images/WikiGO-landing-page.jpg",
+    appLink: "https://tektriage.herokuapp.com/",
+    repoLink: "https://github.com/jjocelynn/WikiGo",
+  },
+  {
+    title: "TekTriage",
+    image: "https://github.com/shaynefw/TekTriage/raw/main/public/images/localhost_3001_.png",
+    appLink: "https://tektriage.herokuapp.com/",
+    repoLink: "https://github.com/shaynefw/TekTriage",
+  },
+  {
+    title: "Travelers Choice",
+    image: "https://user-images.githubusercontent.com/121307266/223154808-03533124-135f-444a-b969-620b85cb13de.png",
+    appLink: "https://travelerschoice.herokuapp.com/",
+    repoLink: "https://github.com/shaynefw/TravelersChoice",
+  },
+  {
+    title: "Matrix Planner",
+    image: "https://github.com/shaynefw/05-matrix-planner/raw/main/assets/images/Your%20Day%20Planner%20(1).png",
+    appLink: "https://shaynefw.github.io/05-matrix-planner/",
+    repoLink: "https://github.com/shaynefw/05-matrix-planner",
+  },
+  {
+    title: "Avenger EDB",
+    image: "https://github.com/shaynefw/12-Avenger-EDB/blob/main/assets/images/12-Avenger-EDB.gif?raw=true",
+    appLink: "https://drive.google.com/file/d/1OfQg3CRzsNXprMWpL54LP8hOMfWIBtVe/view?usp=sharing",
+    repoLink: "https://github.com/shaynefw/12-Avenger-EDB",
+  },
+];
+
+const Portfolio = () => {
+  return (
+    <section className="portfolio">
+      <h2 className="text-center">Portfolio</h2>
+      <div className="projects-container">
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            title={project.title}
+            image={project.image}
+            appLink={project.appLink}
+            repoLink={project.repoLink}
+          />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
